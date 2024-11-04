7f3cd76ec57afffee2b0e852378659032a3ed767
Rupesh-bhagat96 <rupeshnbhagat96@gmail.com>
2024-11-04 18:50:52 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
-import { Collapse } from 'react-bootstrap'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
 import Products from './Pages/Products'
