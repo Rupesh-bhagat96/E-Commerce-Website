12018a63da89c6411f9f9aa4d0377fdbb063d392
Rupesh-bhagat96 <rupeshnbhagat96@gmail.com>
2024-11-04 18:46:27 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react-bootstrap']
-    }
-  },
+  
   plugins: [react()],
 })
 
